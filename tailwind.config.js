950332290f168e3ad2919eba50aafbfe81d017bb
Satya Kresna <satyakresna6295@gmail.com>
2020-02-18 21:44:39 +0800

    feat: footer navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
     backgroundColor: theme => ({
       'midnight-blue': '#1b1c6f'
     }),
+    boxShadow: {
+      'midnight-blue': 'rgba(15, 2, 49, 0.35) 0px 5px 5px inset'
+    }
   },
   variants: {},
   plugins: []
