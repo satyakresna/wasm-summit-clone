1d2ea1171209ffeb8cd0da3477254809ac490ff3
Satya Kresna <satyakresna6295@gmail.com>
2020-02-20 04:54:16 +0800

    feat: hide navbar on scroll

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,16 @@
 document.onreadystatechange = function () {
   if (document.readyState === 'complete') {
-    page('/', index);
-    // Call it!
-    page();
-
-    function index() {
-      document.querySelector('main').textContent = 'Index';
-    }
-
+    let prevScrollPos = window.scrollY;
+    window.addEventListener('scroll', function (e) {
+      window.requestAnimationFrame(function() {
+        let currentScrollPos = window.scrollY;
+        if (prevScrollPos > currentScrollPos) {
+          document.getElementById('navbar-bottom').style.cssText = "transform: none; transition: all 150ms ease 0s;";
+        } else {
+          document.getElementById('navbar-bottom').style.cssText = "transform: translateY(100%); transition: all 150ms ease 0s;";
+        }
+        prevScrollPos = currentScrollPos;
+      })
+    })
   }
 }
