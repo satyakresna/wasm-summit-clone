63dc050dc3262f8386d0f72a70bcfc02923424e6
Satya Kresna <satyakresna6295@gmail.com>
2020-02-20 20:54:18 +0800

    fix(scrollcontent): run if navbar bottom is not null

--- src/js/behaviours/scrollContent.js
+++ src/js/behaviours/scrollContent.js
@@ -1,14 +1,16 @@
 export default function scrollContent() {
-  let prevScrollPos = window.scrollY;
-  window.addEventListener('scroll', function (e) {
-    window.requestAnimationFrame(function() {
-      let currentScrollPos = window.scrollY;
-      if (prevScrollPos > currentScrollPos) {
-        document.getElementById('navbar-bottom').style.cssText = "transform: none; transition: all 150ms ease 0s;";
-      } else {
-        document.getElementById('navbar-bottom').style.cssText = "transform: translateY(100%); transition: all 150ms ease 0s;";
-      }
-      prevScrollPos = currentScrollPos;
-    })
-  })
+  if (document.getElementById('navbar-bottom') !== null) {
+    let prevScrollPos = window.scrollY;
+    window.addEventListener('scroll', function (e) {
+      window.requestAnimationFrame(function() {
+        let currentScrollPos = window.scrollY;
+        if (prevScrollPos > currentScrollPos) {
+          document.getElementById('navbar-bottom').style.cssText = "transform: none; transition: all 150ms ease 0s;";
+        } else {
+          document.getElementById('navbar-bottom').style.cssText = "transform: translateY(100%); transition: all 150ms ease 0s;";
+        }
+        prevScrollPos = currentScrollPos;
+      })
+    }) 
+  }
 }
