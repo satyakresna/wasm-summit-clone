7f251d0d5b0d0479f6383ca89e4a4a41e4d1c09f
Satya Kresna <satyakresna6295@gmail.com>
2020-02-22 22:26:37 +0800

    fix(rename): speaker to speakers file

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 import scrollContent from "./behaviours/scrollContent.js";
 import clickHamburgerMenu from "./behaviours/clickHamburgerMenu.js";
 import Home from "./pages/home.js";
-import Speaker from "./pages/speaker.js";
+import Speakers from "./pages/speakers.js";
 import Schedule from "./pages/schedule.js";
 import About from "./pages/about.js";
 import NotFound from "./pages/notfound.js";
@@ -9,7 +9,7 @@ import NotFound from "./pages/notfound.js";
 document.onreadystatechange = function () {
   if (document.readyState === 'complete') {
     page('/', Home);
-    page('/speakers', Speaker);
+    page('/speakers', Speakers);
     page('/schedule', Schedule);
     page('/about', About);
     page('*', NotFound);
