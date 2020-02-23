8aabfa674bb8b811da1c6862290337c5396fc85e
Satya Kresna <satyakresna6295@gmail.com>
2020-02-23 14:34:21 +0800

    feat: display detail of speaker

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import scrollContent from "./behaviours/scrollContent.js";
 import clickHamburgerMenu from "./behaviours/clickHamburgerMenu.js";
 import Home from "./pages/home.js";
 import Speakers from "./pages/speakers.js";
+import Speaker from "./pages/speaker.js";
 import Schedule from "./pages/schedule.js";
 import About from "./pages/about.js";
 import NotFound from "./pages/notfound.js";
@@ -10,6 +11,7 @@ document.onreadystatechange = function () {
   if (document.readyState === 'complete') {
     page('/', Home);
     page('/speakers', Speakers);
+    page('/speakers/:speaker', Speaker);
     page('/schedule', Schedule);
     page('/about', About);
     page('*', NotFound);
