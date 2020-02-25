4bd53a0da52a000b9c0df6ae8b612cee0aa216f1
Satya Kresna <satyakresna6295@gmail.com>
2020-02-25 20:08:14 +0800

    feat: about page

---
+++ src/data/about.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    question: "How much does it cost?",
+    answer: "Given you have a ticket, attending the event itself is free. At this point in time, we do not cover travel expenses or offer scholarships. If that should change we will announce it."
+  },
+  {
+    question: "How can I get a ticket?",
+    answer: "The application for tickets has already closed. If you had applied to participate in the ticket raffle for the event, you will get a notification via e-mail once you got a ticket. Note that there are several waves of invitations depending on when tickets become available."
+  },
+  {
+    question: "Can I transfer my ticket to someone else?",
+    answer: `If you got a ticket but want to transfer it to a person that is more suitable to attend please send us an email to wasm-summit-2020@chromium.org with the new attendee's name and email and we will transfer the ticket.`
+  },
+  {
+    question: "Is this a community event?",
+    answer: "Yes, the summit is organised by individual people in the WebAssembly community, and at the time of writing sponsored by Google and Mozilla."
+  },
+  {
+    question: "Who is organizing this?",
+    answer: "The organizers are (in alphabetical order): Aaron Turner, Ashley Williams, Michael Hablich, Surma, Thomas Tränkler"
+  },
+  {
+    question: "How do I contact you?",
+    answer: "If you need to get in contact with the organizers for whatever reason, please send an email to wasm-summit-2020@chromium.org. Thanks for all the support from the WebAssembly community!"
+  },
+  {
+    question: "How do I find you on Twitter?",
+    answer: "The official hashtag of the conference is #WasmSummit. There is no dedicated Twitter account for the conference."
+  },
+  {
+    question: "Do you have a live chat?",
+    answer: "The official chat channel of the conference is the #summit channel on the WebAssembly Discord. Here you can exchange with the organizers or other attendees before, during or after the conference."
+  },
+  {
+    question: "Will you have a livestream?",
+    answer: "Yes, the summit will be livestreamed on our Youtube channel. So no worries, even if you are not able to attend, you will be able to follow the event."
+  },
+  {
+    question: "Will you record the talks?",
+    answer: "Yes, the talks will be uploaded on our Youtube channel. So no worries, even if you cannot attend or follow the livestream, you will be able to catch up on the talks later once they are published."
+  },
+  {
+    question: "Which hotels do you recommend?",
+    answer: "Hotels can be expensive in Mountain View. AirBnB is a moderately priced option with offers around the conference site."
+  },
+  {
+    question: "Do you have a code of conduct?",
+    answer: "WebAssembly Summit is intended an inclusive conference for everyone and will enforce our code of conduct to make sure everybody feels welcome and safe. On-site, conference staff will be identifiable by their Wasm purple tshirts with 'STAFF' on their back. If you need help of any kind, to report a bad situation, or anything else not specified that we can help with, please find us on site, DM us on twitter, or call/text."
+  }
+]
--- src/js/pages/about.js
+++ src/js/pages/about.js
@@ -1,3 +1,32 @@
+import renderNavbarBottom from "../components/renderNavbarBottom.js";
+import renderNavbarPopup from "../components/renderNavbarPopup.js";
+import about from "../../data/about.js";
+
 export default function () {
   document.body.textContent = '';
+
+  const $faq = document.createElement('div');
+  $faq.setAttribute('style', 'break-inside: avoid;');
+  const fragment = document.createDocumentFragment();
+  for (const faq of about) {
+    const $content = document.createRange().createContextualFragment(`
+      <h2 class="text-3xl font-bold py-15px pr-15px pl-5px border-b-3 border-solid" style="border-bottom-color: rgba(255, 255, 255, 0.4);">${faq.question}</h2>
+      <p class="text-xl mt-25px px-15px pb-50px leading-relaxed" style="text-shadow: rgba(0, 0, 0, 0.25) 1px 4px 10px;">${faq.answer}</p>
+    `);
+    fragment.appendChild($content);
+  }
+  $faq.appendChild(fragment);
+
+  const $content = document.createRange().createContextualFragment(`
+    <div class="flex fixed inset-0" style="background-color: rgba(122, 122, 122, 0.15); z-index: -1;"></div>
+    ${renderNavbarBottom()}
+    ${renderNavbarPopup()}
+    <div style="padding: 3vh 9vw">
+      <h1 class="text-5xl font-bold p-10px" style="margin-top: 3vh; margin-bottom: 0vh;">About</h1>
+      <div style="columns: 500px auto; column-gap: 10vw;">
+        ${$faq.outerHTML}
+      </div>
+    </div>
+  `);
+  document.body.appendChild($content);
 }
